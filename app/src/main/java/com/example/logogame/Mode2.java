c8f99ecc87b8dfd65821a927826b3e278ba4bfee
Kenton Duprey <kdtech18@gmail.com>
2021-02-20 17:50:33 -0500

    adding buttons and mode activities

--- app/src/main/java/com/example/logogame/Mode2.java
+++ app/src/main/java/com/example/logogame/Mode2.java
@@ -2,18 +2,13 @@ package com.example.logogame;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class Mode2 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
-
-    public void startMode(View view) {
+        setContentView(R.layout.activity_mode2);
     }
 }
