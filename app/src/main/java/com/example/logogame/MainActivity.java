c408843291b13f0ec0f133c1b714783364b63f27
Kenton Duprey <kdtech18@gmail.com>
2021-02-23 22:41:41 -0500

    Adding randomImg method as well as image assets

--- app/src/main/java/com/example/logogame/MainActivity.java
+++ app/src/main/java/com/example/logogame/MainActivity.java
@@ -5,8 +5,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,6 +16,23 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void startMode(View view) {
+    public void startMode1(View view) {
+        Intent intent = new Intent(MainActivity.this, Mode1.class);
+        startActivity(intent);
+    }
+
+    public void startMode2(View view) {
+        Intent intent = new Intent(MainActivity.this, Mode2.class);
+        startActivity(intent);
+    }
+
+    public void startMode3(View view) {
+        Intent intent = new Intent(MainActivity.this, Mode3.class);
+        startActivity(intent);
+    }
+
+    public void startMode4(View view) {
+        Intent intent = new Intent(MainActivity.this, Mode4.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/logogame/Mode1.java
+++ app/src/main/java/com/example/logogame/Mode1.java
@@ -2,13 +2,35 @@ package com.example.logogame;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.Image;
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import java.util.Random;
 
 public class Mode1 extends AppCompatActivity {
+
+    String[] models = {"a", "b", "c", "f", "h", "l", "mc", "me", "mi", "n", "p", "s", "te", "to", "v"};
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mode1);
+        ImageView img = (ImageView) findViewById(R.id.random_img);
+        int id = getRandomImage();
+        img.setImageResource(id);
+
+    }
+
+    public int getRandomImage() {
+        int x = new Random().nextInt(5) + 1;
+        int y = new Random().nextInt(models.length);
+        String randImg = models[y] + x;
+
+        int id = getResources().getIdentifier(randImg, "drawable", getPackageName());
+
+        return id;
+
     }
 }
