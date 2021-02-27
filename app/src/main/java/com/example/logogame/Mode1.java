0829abc49ffdbd7b639af5a29c851d37fa8a87e2
Kenton Duprey <kdtech18@gmail.com>
2021-02-26 20:41:30 -0500

    updating photo file sizes and adding spinner

--- app/src/main/java/com/example/logogame/Mode1.java
+++ app/src/main/java/com/example/logogame/Mode1.java
@@ -4,14 +4,19 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.media.Image;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ImageView;
+import android.widget.Spinner;
 
 import java.util.Random;
 
-public class Mode1 extends AppCompatActivity {
+public class Mode1 extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
-    String[] models = {"a", "b", "c", "f", "h", "l", "mc", "me", "mi", "n", "p", "s", "te", "to", "v"};
+    private String[] models = {"a", "b", "c", "f", "h", "l", "mc", "me", "mi", "n", "p", "s", "te", "to", "v"};
 
+    private String userSelection = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +25,18 @@ public class Mode1 extends AppCompatActivity {
         ImageView img = (ImageView) findViewById(R.id.random_img);
         int id = getRandomImage();
         img.setImageResource(id);
+
+        Spinner carSpinner = findViewById(R.id.model_list_spinner);
+        if (carSpinner != null) {
+            carSpinner.setOnItemSelectedListener(this);
+        }
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.models_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        if (carSpinner != null) {
+            carSpinner.setAdapter(adapter);
+        }
 
     }
 
@@ -31,6 +48,17 @@ public class Mode1 extends AppCompatActivity {
         int id = getResources().getIdentifier(randImg, "drawable", getPackageName());
 
         return id;
+
+    }
+
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        userSelection = parent.getItemAtPosition(position).toString();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
 
     }
 }
